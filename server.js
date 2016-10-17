4645bfdc9d33ed4827696ff9a62338e367189a91
Qing Zhou <qzhou.pku@gmail.com>
2016-10-17 22:01:06 +0000

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,2 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res) {
-   res.send('Hello World'); 
-});
-app.listen(process.env.PORT,function() {
-    console.log('listening on 8080');
-});
